0a305ef974bfca577f4cd422dbf4143537f8141f
EC2 Default User <ec2-user@ip-172-31-0-86.ap-northeast-1.compute.internal>
2023-03-23 04:48:06 +0000

    kadai4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,17 @@
 class RelationshipsController < ApplicationController
-  
+  before_action :authenticate_user!
   def create
-    current_user.follow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.follow(user)
     redirect_to request.referer
   end
-  
+
   def destroy
-    current_user.unfollow(params[:user_id])
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
     redirect_to request.referer
   end
-  
+
   def followings
     user = User.find(params[:user_id])
     @users = user.followings
--- app/models/user.rb
+++ app/models/user.rb
@@ -23,12 +23,12 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
   
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
+  def follow(user)
+    relationships.create(followed_id: user.id)
   end
   
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
   end
   
   def following?(user)
